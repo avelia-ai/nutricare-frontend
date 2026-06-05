48bd7f0b652ec3ed63a5b2b273e754f7331b348f
avelia-ai <guillaumecousin@hotmail.fr>
2026-06-05 10:48:42 +0000

    revert: annule les changements Android problématiques

--- android/app/src/main/java/fr/mirella/app/MainActivity.java
+++ android/app/src/main/java/fr/mirella/app/MainActivity.java
@@ -1,3 +1,15 @@
 package fr.mirella.app;
+import android.graphics.Color;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-public class MainActivity extends BridgeActivity {}
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Fond coral pendant le chargement de la WebView
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            getBridge().getWebView().setBackgroundColor(Color.parseColor("#FF6B6B"));
+        }
+    }
+}
